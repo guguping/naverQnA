d7a2b291be61c2504517bc292c316bf9308fb8d5
guguping <qkfhandur@naver.com>
2023-05-22 15:05:48 +0900

    boardSave.jsp - 제약조건

---
+++ src/main/java/com/icia/naverQnA/DTO/BoardDTO.java
@@ -0,0 +1,22 @@
+package com.icia.naverQnA.DTO;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Data
+@NoArgsConstructor
+public class BoardDTO {
+    Long id;
+    String boardTitle;
+    String boardWriter;
+    String boardContents;
+    int boardHits;
+    Timestamp boardCreatedDate;
+    int fileAttached = 0;
+    List<MultipartFile> boardFile;
+    Long memberId;
+}
